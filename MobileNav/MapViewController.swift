20cc7862ef031946f98f51ac5fdefb608068f3a2
lapinskijw <jlapinski.dev@gmail.com>
2020-04-03 12:01:57 -0400

    added map view

---
+++ MobileNav/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MobileNav
+//
+//  Created by James Lapinski on 4/3/20.
+//  Copyright © 2020 Livio Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
