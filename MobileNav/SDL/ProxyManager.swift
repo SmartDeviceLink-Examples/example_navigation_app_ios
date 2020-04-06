2ba33cf1971c1bf5e3a70c7f679306fdc9b962b1
lapinskijw <jlapinski.dev@gmail.com>
2020-04-06 17:16:31 -0400

    update to be offscreen, set up proxy manager

--- MobileNav/SDL/ProxyManager.swift
+++ MobileNav/SDL/ProxyManager.swift
@@ -16,20 +16,22 @@ enum ConnectionType {
 
 class ProxyManager: NSObject {
 
-    static let sharedManager = ProxyManager()
     public private(set) var sdlManager: SDLManager!
+    static let sharedManager = ProxyManager()
 
     private override init() {
         super.init()
+        registerForNotifications()
     }
 
     func connect(with connectionType: ConnectionType) {
         sdlManager = SDLManager(configuration: connectionType == .iap ? ProxyManager.connectIAP() : ProxyManager.connectTCP(), delegate:self)
 
         sdlManager.start { (success, error) in
-            guard success else {
-                SDLLog.e("There was an error while starting up: \(String(describing: error))")
-                return
+            if success {
+                // app has succussfully connected
+            } else {
+                print("SDL Connection Error: \(error!)")
             }
         }
     }
@@ -45,24 +47,73 @@ class ProxyManager: NSObject {
     }
 
     class func setupConfiguration(with lifecycleConfiguration: SDLLifecycleConfiguration) -> SDLConfiguration {
-        lifecycleConfiguration.appType = .navigation
-        if let appLogo = UIImage(named: "logo") {
-            let appIcon = SDLArtwork(image: appLogo, name: "logo", persistent: true, as: .PNG)
+        if let appLogo = UIImage(named: "icon") {
+            let appIcon = SDLArtwork(image: appLogo, name: "icon", persistent: true, as: .PNG)
             lifecycleConfiguration.appIcon = appIcon
         }
+        lifecycleConfiguration.appType = .navigation
+        let lockscreenConfig = SDLLockScreenConfiguration.enabled()
+        lockscreenConfig.displayMode = .always
 
-        return SDLConfiguration(lifecycle: lifecycleConfiguration, lockScreen: .enabled(), logging: .default(), streamingMedia:ProxyManager.streamingMediaConfiguration(), fileManager: .default(), encryption: .default())
+        return SDLConfiguration(lifecycle: lifecycleConfiguration, lockScreen: .enabled(), logging: ProxyManager.logConfiguration(), streamingMedia:ProxyManager.streamingMediaConfiguration(), fileManager: .default(), encryption: nil)
+    }
+
+    class func logConfiguration() -> SDLLogConfiguration {
+        let logConfig = SDLLogConfiguration.debug()
+        logConfig.globalLogLevel = .debug
+        return logConfig
     }
 
     class func streamingMediaConfiguration() -> SDLStreamingMediaConfiguration {
-        let streamingMediaConfig = SDLStreamingMediaConfiguration()
 
-        streamingMediaConfig.rootViewController = SDLCarWindowViewController()
+        let mapViewController = UIStoryboard(name: "SDLMapKitMap", bundle: nil).instantiateInitialViewController() as? MapViewController
+        let streamingMediaConfig = SDLStreamingMediaConfiguration .autostreamingInsecureConfiguration(withInitialViewController: mapViewController!)
         streamingMediaConfig.carWindowRenderingType = .viewAfterScreenUpdates
 
         return streamingMediaConfig
     }
 
+}
+
+private extension ProxyManager {
+    func registerForNotifications() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(sdlVideoStreamDidStart), name: .SDLVideoStreamDidStart, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(sdlVideoStreamDidStop), name: .SDLVideoStreamDidStop, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(sdlVideoStreamSuspended), name: .SDLVideoStreamSuspended, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(sdlAudioStreamDidStart), name: .SDLAudioStreamDidStart, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(sdlAudioStreamDidStop), name: .SDLAudioStreamDidStop, object: nil)
+    }
+
+    @objc func sdlVideoStreamDidStart() {
+        DispatchQueue.main.async {
+            SDLLog.d("Starting video")
+        }
+    }
+
+    @objc func sdlVideoStreamDidStop() {
+        DispatchQueue.main.async {
+            SDLLog.d("Stopping video")
+        }
+    }
+
+    @objc func sdlVideoStreamSuspended() {
+        DispatchQueue.main.async {
+            SDLLog.d("Suspending video")
+        }
+    }
+
+    @objc func sdlAudioStreamDidStart() {
+        DispatchQueue.main.async {
+            SDLLog.d("Starting audio")
+        }
+    }
+
+    @objc func sdlAudioStreamDidStop() {
+        DispatchQueue.main.async {
+             SDLLog.d("Stopping audio")
+        }
+    }
 }
 
 extension ProxyManager: SDLManagerDelegate {
@@ -72,9 +123,13 @@ extension ProxyManager: SDLManagerDelegate {
 
     func hmiLevel(_ oldLevel: SDLHMILevel, didChangeToLevel newLevel: SDLHMILevel) {
         if newLevel != .none {
-            UIApplication.shared.isIdleTimerDisabled = true
+            DispatchQueue.main.async {
+                UIApplication.shared.isIdleTimerDisabled = true
+            }
         } else {
-            UIApplication.shared.isIdleTimerDisabled = false
+            DispatchQueue.main.async {
+                UIApplication.shared.isIdleTimerDisabled = false
+            }
         }
     }
 }
